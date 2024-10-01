bea83c7d8e64a2a9c11e0ac22f8e4f7e12046ece
fcu2134 <facundo.marin.moscol@cftmail.cl>
2024-09-30 20:21:50 -0600

    Agregar archivos de proyecto.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace paty.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
